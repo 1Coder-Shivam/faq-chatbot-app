94fb733e0c73c030f0e409667080c1e1600664a0
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2025-04-27 16:06:45 +0530

    frontend updated host preview fix 11

--- faq-chat-bot-frontend/vite.config.js
+++ faq-chat-bot-frontend/vite.config.js
@@ -6,13 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    proxy: {
-      '/api': {
-        target: "https://faq-chatbot-app-backend.onrender.com",
-        changeOrigin: true,
-        secure: false
-      }
-    }
   },
   preview: {
     host: '0.0.0.0', // ✅ allow external traffic
